06b199ba5ed5f37058d6e611269200a8991003a6
Bang <seungilbang@khu.ac.kr>
2023-05-21 19:55:16 +0900

    initial commit

---
+++ src/main/java/com/webdev/spring/JavaWebDev4Application.java
@@ -0,0 +1,13 @@
+package com.webdev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDev4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebDev4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/webdev/spring/JavaWebDev4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebDev4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
